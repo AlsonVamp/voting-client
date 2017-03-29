6fca2c1e264a346c6a59ac1296e696c4e62c765e
Alexander Nazarov <alsnazarov@gmail.com>
2017-03-29 14:10:34 +0300

    Refactor Voting as Pure component with PureRenderMixin

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Vote from './Vote';
 import Winner from './Winner';
 
 export default class Voting extends React.Component {
+    constructor(props) {
+        super(props);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
     render() {
         return <div>
             {this.props.winner ?

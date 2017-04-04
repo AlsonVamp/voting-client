f45fac098dda5d14f1c7c678b6f895ae2ff90225
Alexander Nazarov <alsnazarov@gmail.com>
2017-04-04 15:01:46 +0300

    Add react router 4 and Results component

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
-const pair = ['Trainspotting', '28 Days Later'];
-
+const AppVoting = () => <App><Voting /></App>;
+const AppResults = () => <App><Results /></App>;
 ReactDOM.render(
-    <Voting pair={pair} winner='Trainspotting' />,
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/results">Results</Link></li>
+            </ul>
+            <Route exact path="/" component={AppVoting} />
+            <Route path="/results" component={AppResults} />
+        </div>
+    </Router>,
     document.getElementById('app')
 );

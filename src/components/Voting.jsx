137b1ab3bfb786056a90c65e3e33c90635fd735d
Alexander Nazarov <alsnazarov@gmail.com>
2017-04-10 11:06:50 +0300

    Add action creators and get state  via socket.io

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { connect } from 'react-redux';
+
+import * as actionCreators from '../action_creators';
 import Vote from './Vote';
 import Winner from './Winner';
 
@@ -21,9 +23,10 @@ export class Voting extends React.Component {
 function mapStateToProps(state) {
     return {
         pair: state.getIn(['vote', 'pair']),
+        hasVoted: state.get('hasVoted'),
         winner: state.get('winner')
     };
 }
 
-const VotingContainer = connect(mapStateToProps)(Voting);
+const VotingContainer = connect(mapStateToProps, actionCreators)(Voting);
 export default VotingContainer;
--- src/index.jsx
+++ src/index.jsx
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import io from 'socket.io-client';
+
+import reducer from './reducer';
+import { setState } from './action_creators';
 import App from './components/App';
 import VotingContainer from './components/Voting';
 import ResultsContainer from './components/Results';
 
 const store = createStore(reducer);
-store.dispatch({
-    type: 'SET_STATE',
-    state: {
-        vote: {
-            pair: ['Sunshine', '28 Days Later'],
-            tally: { Sunshine: 2 }
-        }
-    }
-});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+    store.dispatch(setState(state))
+);
 
 const AppVoting = () => <App><VotingContainer /></App>;
 const AppResults = () => <App><ResultsContainer /></App>;
